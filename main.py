6df1cdc5cbc340aec472f1054410035b4d9ad87c
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-07-08 15:03:20 -0500

    Conditionals

--- main.py
+++ main.py
@@ -115,4 +115,41 @@ print(new_list)
 # DICTIONARY or MAP -------------------------------------------------
 # Made up of values with a unique key for each value
 # Similar to lists, but you can't join dicts with a +
+
+super_villains = {'Fiddler' : 'Isaac Bowin',
+                  'Captain Cold' : 'Leonard Snart',
+                  'Weather Wizard' : 'Mark Mardon',
+                  'Mirror Master' : 'Sam Scudder',
+                  'Pied Piper' : 'Thomas Peterson'}
+ 
+print(super_villains['Captain Cold'])
+ 
+# Delete an entry
+del super_villains['Fiddler']
+print(super_villains)
+
+# Replace a value
+super_villains['Mirror Master'] = 'Hartley Rathaway'
+print(super_villains)
+
+# Print the number of items in the dictionary
+print(len(super_villains))
+
+# Get the value for the passed key
+print(super_villains.get("Pied Piper"))
+
+# Get a list of dictionary keys
+print(super_villains.keys())
+
+# Get a list of dictionary values
+print(super_villains.values())
+
+# CONDITIONALS -------------------------------------------------------------
+# The if, else and elif statements are used to perform different
+# actions based off of conditions
+# Comparison Operators : ==, !=, >, <, >=, <=
+ 
+# The if statement will execute code if a condition is met
+# White space is used to group blocks of code in Python
+# Use the same number of proceeding spaces for blocks of code
  
